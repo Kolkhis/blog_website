aba31e242ec541c4a683a0686c4cfb3222a27d14
Kolkhis <connor.wilkins@outlook.com>
2023-05-21 15:21:02 -0400

    Remove MIMEMultipart dependency.

--- email_manager.py
+++ email_manager.py
@@ -1,7 +1,7 @@
 import smtplib
 import os
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
 
 BOT_EMAIL = os.environ['BOT_EMAIL']
 APP_PASSWORD = os.environ['EMAIL_PASSWORD']
@@ -9,19 +9,23 @@ APP_PASSWORD = os.environ['EMAIL_PASSWORD']
 
 class EmailManager:
     def __init__(self):
-        self.message = MIMEMultipart()
+        # self.message = MIMEMultipart()
         self.msg_text = ''
 
     def create_email(self, data: dict):
         """Formats the email as a MIMEMultipart message."""
-        self.message.add_header('subject', f'message from {data["name"]}, a reader!')
-        self.message.add_header('from', data['email'])
-        self.message.add_header('to', BOT_EMAIL)
-        msg_body = MIMEText(_text=f'message from {data["name"]}:\n'
-                                  f'{data["message"]}\n\n' \
-                                  f'phone number: {data["phone"]}\n'
-                                  f'email addr:   {data["email"]}')
-        self.message.attach(msg_body)
+        self.msg_text = f"From:{data['email']}\n" \
+                        f"To: {BOT_EMAIL}\n" \
+                        f"Subject: Message from {data['name']}, from the blog site.\n\n" \
+                        f"{data['message']}"
+        # self.message.add_header('subject', f'message from {data["name"]}, a reader!')
+        # self.message.add_header('from', data['email'])
+        # self.message.add_header('to', BOT_EMAIL)
+        # msg_body = MIMEText(_text=f'message from {data["name"]}:\n'
+        #                           f'{data["message"]}\n\n' \
+        #                           f'phone number: {data["phone"]}\n'
+        #                           f'email addr:   {data["email"]}')
+        # self.message.attach(msg_body)
 
     def send_email(self, data: dict):
         """Creates an email from BOT_EMAIL and sends it to myself"""
